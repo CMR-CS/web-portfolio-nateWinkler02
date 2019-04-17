e549fa244ec1534c2fb2735505526802cc27c956
nateWinkler02 <47153510+nateWinkler02@users.noreply.github.com>
2019-04-17 10:35:57 -0600

    4/17
    4/17

---
+++ scripts/pixel.js
@@ -0,0 +1,20 @@
+/*
+ * Auto-generated content from the Brackets New Project extension.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global $, window, document */
+
+// Simple jQuery event handler
+
+var penColour='black';
+
+function setPixelColor(pixel)
+{
+    pixel.style.backgroundColor =penColour;
+}
+
+function setPenColour(pen)
+{
+    penColour = pen;
+}
